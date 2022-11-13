4395107a47ca12b40a1de25b72b5db37e6cc1af5
k021Ng <k021Ng@qq.com>
2022-11-13 17:25:44 +0800

    v1.0.12 add steering behavior

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 # Changelog
 All notable changes to this package will be documented in this file. The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 
+## [1.0.12] - 2022-11-13
+### Added
+- Steering Behavior
+
 ## [1.0.11] - 2022-11-03
 ### Added
 - Capture function
---
+++ Runtime/Scripts/ISteeringAgent.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace QuickDemo
+{
+    public interface ISteeringAgent
+    {
+        float steeringMass { get; }
+        Vector3 steeringPos { get; }
+        Vector3 steeringVelocity { get; }
+        float steeringMaxSpeed { get; }
+        float steeringMaxForce { get; }
+        float steeringCollisionRadius { get; }
+        // Arrival
+        float steeringSlowRadius { get; }
+        float steeringStopRadius { get; }
+        // Avoidance
+        bool steeringNeedAvoid { get; }
+        float steeringAvoidAHead { get; }
+        float steeringAvoidForce { get; }
+        // Separation
+        float steeringSeparationRadius { get; }
+        float steeringSeparationForce { get; }
+    }
+}
