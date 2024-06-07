5c96b14424aa74aad7eb9dca669f24edd350cd09
x44yz <dongl1n777@qq.com>
2024-06-07 16:05:16 +0800

    rename

--- Assets/Scripts/Game/AI/AIdle.cs
+++ Assets/Scripts/Game/AI/AIdle.cs
@@ -6,8 +6,6 @@ using AI.Utility;
 [CreateAssetMenu(fileName = "1010A_Idle", menuName = "AI/A/AIdle")]
 public class AIdle : ActorAction
 {
-    public override System.Type ActionObjType() => typeof(AIdleObj);
-
     public float minMins;
     // public StatVal[] statSpdVals;
 }
